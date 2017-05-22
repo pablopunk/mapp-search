a2dc4fa50164938dbd3c319d4d02ffaa6a5fe047
Pablo Varela <pablovarela182@gmail.com>
2017-05-21 21:41:40 -0400

    Remove app-store-sraper (fix #1)

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const store = require('app-store-scraper')
+const got = require('got')
+
+const itunesUrl = 'https://itunes.apple.com/search?media=software&entity=macSoftware'
+
+const getResults = async (name, limit) => {
+  const url = `${itunesUrl}&term=${name}&limit=${limit}`
+  const response = await got(url)
+  const results = JSON.parse(response.body).results
+  return results
+}
 
 const search = async (name, { number, all } = {number: 1, all: false}) => {
   if (number < 1) {
@@ -6,11 +15,8 @@ const search = async (name, { number, all } = {number: 1, all: false}) => {
   } else if (number && all && number !== 1) {
     throw Error(`Seriously? you want all results or just ${number}?`)
   }
-  const results = await store.search({ term: name, device: store.device.MAC })
-  if (all) {
-    number = results.length
-  }
-  const ids = results.slice(0, number).map(result => result.id)
+  const results = await getResults(name, all ? 50 : number)
+  const ids = results.map(result => result.trackId)
   if (ids.length === 1) {
     return ids[0] // single result
   }
